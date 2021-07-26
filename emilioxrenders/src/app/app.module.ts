c69e6b92cb6f4bfce6f3ac3efe5d9360646a4ff9
emilioxrenders <74618145+emilioxrenders@users.noreply.github.com>
2021-07-26 13:45:33 +0200

    created-layout

--- emilioxrenders/src/app/app.module.ts
+++ emilioxrenders/src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { MatIconModule } from '@angular/material/icon';
+import { WorkComponent } from './work/work.component';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faLinkedinIn, faGithub, fab } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, fas } from '@fortawesome/free-solid-svg-icons';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    SkillsComponent,
+    WorkComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private library: FaIconLibrary) {
+    library.addIconPacks(fab, fas);
+    library.addIcons(faGithub, faLinkedinIn, faEnvelope);
+  }
+}
