22ac3a048f9e8025bb23b3fda1db9565afde4768
emilioxrenders <74618145+emilioxrenders@users.noreply.github.com>
2021-07-27 17:19:01 +0200

    added-aos

--- emilioxrenders/src/app/app.component.ts
+++ emilioxrenders/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'emilioxrenders';
 
-  constructor() { }
+  constructor() {
+    AOS.init();
+  }
 }
