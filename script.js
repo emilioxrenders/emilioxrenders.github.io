4ec4eb72307d989564e0058bde6556836f67033e
emilioxrenders <115925337+emilioxrenders@users.noreply.github.com>
2023-08-12 22:25:51 +0200

    smallChanges

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const nav = document.getElementById("mobile-button-open");
+const nav = document.getElementById("nav-toggle");
+const navOpen = document.getElementById("nav-open");
+const navClose = document.getElementById("nav-close");
 
 nav.addEventListener("click", function() {
   nav.classList.toggle("active");
+  navOpen.classList.toggle("hidden");
+  navClose.classList.toggle("hidden");
 });
